bc911d4f57bb292f5ff633ff6b254228c69104c0
Dhinesh-Kumar-1209 <dhineshkumars395@gmail.com>
2025-04-02 05:30:08 +0530

    Update streamlit_app.py
    connected changes needed bc of move from SIS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 helpful_links = [
@@ -9,6 +8,8 @@ helpful_links = [
     "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
     "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
 ]
+cnx=st.connection("snowflake")
+session=cnx.session()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
