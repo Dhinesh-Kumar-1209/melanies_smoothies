aae184991a9c515821787ba7463266ce661de372
Dhinesh-Kumar-1209 <dhineshkumars395@gmail.com>
2025-04-02 06:08:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,12 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+' '+name_on_order+'!', icon="✅")
 
 
+# New Section to display
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
 
 
 
